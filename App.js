fdc7866cc400fcf4a7dd36ddc68b403fd7cef950
Akmal79 <109073778+Akmal79@users.noreply.github.com>
2024-01-18 23:45:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+// //import logo from './logo.svg';
+// i//mport './App.css';
+
+// /*function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// //export default App;*/
+
+//import Example from "./react1";
+import Demo from "./inline";
+import React from "react";  
+function App()
+{
+  return (
+  <Demo />)
+    }
+
+export default App;
